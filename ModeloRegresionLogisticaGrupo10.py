126fdffaab848e00eb72e4795975a5b9a1ca2f1c
Lucas Prieto <lucasprieto30@gmail.com>
2023-08-27 17:49:38 -0300

    SMOTE para balanceo de datos, 30% de test y resultados

--- ModeloRegresionLogisticaGrupo10.py
+++ ModeloRegresionLogisticaGrupo10.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[54]:
+# In[1]:
 
 
 import pandas as pd
 import numpy as np
 from sklearn import linear_model
 from sklearn import model_selection
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 import seaborn as sb
+from imblearn.over_sampling import SMOTE
 get_ipython().run_line_magic('matplotlib', 'inline')
 data = pd.read_csv('datasetfinal.csv')
 
@@ -28,55 +30,58 @@ dataframe = encoded_data
 dataframe.head()
 
 
-# In[55]:
+# In[2]:
 
 
 dataframe.describe()
 
 
-# In[56]:
+# In[3]:
 
 
 print(dataframe.groupby('fatal').size())
 
 
-# In[57]:
+# In[4]:
 
 
 dataframe.drop(['fatal'],axis=1).hist()
 plt.show()
 
 
-# In[60]:
+# In[ ]:
 
 
 sb.pairplot(dataframe.dropna(), hue='fatal',height=12,vars=["edad","momento_hecho_Diurno","momento_hecho_Nocturno","sexo_Femenino","sexo_Masculino","clase_victima_Conductor","clase_victima_Pasajero","clase_victima_Peaton","vehiculo_victima_Automovil","tipo_incidente_Colision vehiculo/Persona","tipo_incidente_Colision vehiculo/Vehiculo","clima_Bueno"],kind='reg')
 
 
-# In[61]:
+# In[26]:
 
 
 X = np.array(dataframe.drop(['fatal'],axis=1))
 y = np.array(dataframe['fatal'])
-X.shape
+# Aplicar SMOTE para balancear los datos
+smote = SMOTE(random_state=42)
+X_resampled, y_resampled = smote.fit_resample(X, y)
+
+X_train, X_test, y_train, y_test = train_test_split(X_resampled, y_resampled, test_size=0.3, random_state=42)
+X_resampled.shape
 
 
-# In[63]:
+# In[27]:
 
 
-model = linear_model.LogisticRegression(max_iter=2000)
-model.fit(X,y)
+# Crear y entrenar el modelo de regresión logística
+model = linear_model.LogisticRegression(max_iter=9000)
+model.fit(X_train, y_train)
 
+# Realizar predicciones en el conjunto de prueba
+y_pred = model.predict(X_test)
 
-# In[64]:
+# Evaluar el modelo
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Precisión del modelo: {accuracy:.2f}')
 
-
-predictions = model.predict(X)
-print(predictions[:30])
-
-
-# In[66]:
-
-
-model.score(X,y)
+report = classification_report(y_test, y_pred)
+print('Reporte de clasificación:\n', report)
 
